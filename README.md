79011d058d0da9eeb3ed669f1eb9a8d11f112c11
alex <aleksejkosarevskij@gmail.com>
2025-05-20 20:01:05 +0200

    Chipmunk compiling script for both Linux and Windows. Updated README.md

--- README.md
+++ README.md
@@ -4,25 +4,44 @@ Nobody is going to read this so I am not adding full instructions on how to inst
 ### Dependencies
 `
 SDL2
-chipmunk
+make
+cmake
 `
+To build and install chipmunk library run
+```shell
+./scripts/build-chipmunk.sh
+```
+On arch linux there's a `chipmunk` package in AUR but it is built there in Release mode wich doesn't work for me.
+`chipmunk-devel` on Fedora and `libchipmunk-dev` on Debian should be fine.
 ### Compiling
-To build, use `make`
+```shell
+make
+```
+Binary will appear in `build` directory. To build and run there's a target `run` in `Makefile`:
+```shell
+make run
+```
+I use Arch btw and had the problem: `./build/particles: error while loading shared libraries: libchipmunk.so.7: cannot open shared object file: No such file or directory`. To fix this, I had to set up a `LD_LIBRARY_PATH` variable:
+```shell
+export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
+```
 ## Build on Linux for Windows
 ### Dependencies
 `
 mingw-w64
 cmake
+make
 zip
 `
 ### Compiling
 Warning: be careful with the `$(nproc)` used in `$MAKEFLAGS` variable.
 ```shell
 export MAKEFLAGS=$(nproc)
-bash ./scripts/build-chipmunk.sh
-bash ./scripts/build-sdl2.sh
+HOST=Windows ./scripts/build-chipmunk.sh
+HOST=Windows ./scripts/build-sdl2.sh
 ```
-These scripts will build dll for Windows
+These scripts will build dll's for Windows. Then build the project:
 ```
 make windows
 ```
+After this, `particles.zip` must appear in `build` directory. On Windows machine UNPACK this before launching. Don't launch it straightly from the archive.
--- scripts/build-chipmunk.sh
+++ scripts/build-chipmunk.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 ###
-# This script builds chipmunk for Windows. 
-# If you want to install for linux, then search it on repositories.
+# This script builds chipmunk for Windows if you run it with an env variable
+# $HOME set to Windows, like:
+# HOME=Windows ./scripts/build-chipmunk.sh
 #
-# AUR: chipmunk
-# Fedora: chipmunk-devel
-# Debian (checked): libchipmunk-dev
+# Otherwise it will build and install this library for linux with sudo.
 ###
 
 set -e
@@ -15,6 +14,22 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 CHIPMUNK_DIR="$(realpath ${SCRIPT_DIR}/..)/3rdparty/chipmunk"
 SRC_DIR="${CHIPMUNK_DIR}/src"
 INSTALL_DIR="${CHIPMUNK_DIR}/install"
+
+if [[ $HOST == Windows ]]; then
+    WINDOWS_PARAMS=" \
+        -DCMAKE_SYSTEM_NAME=Windows \
+        -DCMAKE_C_COMPILER=x86_64-w64-mingw32-gcc \
+        -DCMAKE_CXX_COMPILER=x86_64-w64-mingw32-g++ \
+        -DCMAKE_EXE_LINKER_FLAGS=\"-lpthread\" \
+        -DCMAKE_SHARED_LINKER_FLAGS=\"-lpthread\" \
+        -DBUILD_SHARED=OFF \
+        -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} \
+    "
+else
+    WINDOWS_PARAMS=" \
+        -DBUILD_SHARED=ON \
+    "
+fi
 
 mkdir -pv "${CHIPMUNK_DIR}"
 
@@ -29,19 +44,18 @@ cd build
 
 cmake .. \
     --fresh \
-    -DCMAKE_SYSTEM_NAME=Windows \
     -DBUILD_DEMOS=OFF \
-    -DBUILD_SHARED=OFF \
-    -DCMAKE_C_COMPILER=x86_64-w64-mingw32-gcc \
-    -DCMAKE_CXX_COMPILER=x86_64-w64-mingw32-g++ \
-    -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} \
     -DCMAKE_BUILD_TYPE=Debug \
     -DNDEBUG=ON \
-    -DCMAKE_EXE_LINKER_FLAGS="-lpthread" \
-    -DCMAKE_SHARED_LINKER_FLAGS="-lpthread"
+    $WINDOWS_PARAMS
 
 make
-make install
+if [[ $HOST == Windows ]]; then
+    make install
+else
+    echo -e "\nInstalling Chipmunk library."
+    sudo make install
+fi
 
 # rm -rf ${SRC_DIR}
 echo ------------------------------------------------------------
